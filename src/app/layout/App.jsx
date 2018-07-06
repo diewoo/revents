5ee7189a686073211b58c31cb42e7244dbb3ce33
Diego Renteria <diego.renteria@BCTSCONSULT.COM>
2018-07-06 18:22:53 -0500

    3%

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import EventDashboard from '../../features/events/EventDashboard/EventDashboard'
 
 
 class App extends Component {
@@ -7,6 +7,7 @@ class App extends Component {
     return (
       <div >
         <h1>Re-events</h1>
+        <EventDashboard/>
       </div>
     );
   }
---
+++ src/features/events/EventDashboard/EventDashboard.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+//import { Grid } from 'semantic-ui-react'
+class EventDashboard extends Component {
+  render() {
+    return (
+        <div className="ui grid">
+        <div>a</div>
+        <div>a</div>
+      </div>
+    )
+  }
+}
+
+
+
+export default EventDashboard;
