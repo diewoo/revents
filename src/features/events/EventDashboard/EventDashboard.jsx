3a58d589dc5c3b7417596ca3dddf47e613450439
diewoo <dgo250594@gmail.com>
2018-07-09 23:40:09 -0500

    no bind

--- src/features/events/EventDashboard/EventDashboard.jsx
+++ src/features/events/EventDashboard/EventDashboard.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { Grid, Button } from "semantic-ui-react";
+import React, {Component} from "react";
+import {Grid, Button} from "semantic-ui-react";
 import EventList from "../EventList/EventList";
 import EventForm from "../EventForm/EventForm";
 
@@ -9,8 +9,9 @@ const Eventdashboard = [
     title: "Trip to Tower of London",
     date: "2018-03-27T11:00:00+00:00",
     category: "culture",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin " +
+        "ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu" +
+        " vehicula ullamcorper.",
     city: "London, UK",
     venue: "Tower of London, St Katharine's & Wapping, London",
     hostedBy: "Bob",
@@ -20,21 +21,20 @@ const Eventdashboard = [
         id: "a",
         name: "Bob",
         photoURL: "https://randomuser.me/api/portraits/men/20.jpg"
-      },
-      {
+      }, {
         id: "b",
         name: "Tom",
         photoURL: "https://randomuser.me/api/portraits/men/22.jpg"
       }
     ]
-  },
-  {
+  }, {
     id: "2",
     title: "Trip to Punch and Judy Pub",
     date: "2018-03-28T14:00:00+00:00",
     category: "drinks",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin " +
+        "ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu" +
+        " vehicula ullamcorper.",
     city: "London, UK",
     venue: "Punch & Judy, Henrietta Street, London, UK",
     hostedBy: "Tom",
@@ -44,8 +44,7 @@ const Eventdashboard = [
         id: "b",
         name: "Tom",
         photoURL: "https://randomuser.me/api/portraits/men/22.jpg"
-      },
-      {
+      }, {
         id: "a",
         name: "Bob",
         photoURL: "https://randomuser.me/api/portraits/men/20.jpg"
@@ -55,15 +54,26 @@ const Eventdashboard = [
 ];
 
 class EventDashboard extends Component {
+
+  state = {
+    events: Eventdashboard,
+    isOpen: false
+  };
+
+  handleFormOpen = () => {
+    this.setState({isOpen: true});
+  };
+  handleCancel = () => {
+    this.setState({isOpen: false});
+  };
   render() {
     return (
       <Grid>
         <Grid.Column width={10}>
-          <EventList events={Eventdashboard}/>
+          <EventList events={this.state.events}/>
         </Grid.Column>
         <Grid.Column width={6}>
-          <Button positive content="Create Event" />
-          <EventForm />
+          <Button onClick={this.handleFormOpen} positive content="Create Event"/> {this.state.isOpen && <EventForm handleCancel={this.handleCancel}/>}
         </Grid.Column>
       </Grid>
     );
--- src/features/events/EventForm/EventForm.jsx
+++ src/features/events/EventForm/EventForm.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import { Segment, Form, Button } from "semantic-ui-react";
 class EventForm extends Component {
+
   render() {
+    const {handleCancel}=this.props;
     return (
       <div>
         <Segment>
@@ -29,7 +31,7 @@ class EventForm extends Component {
             <Button positive type="submit">
               Submit
             </Button>
-            <Button type="button">Cancel</Button>
+            <Button onClick={handleCancel} type="button">Cancel</Button>
           </Form>
         </Segment>
       </div>
